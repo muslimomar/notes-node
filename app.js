85715cddae3e290416a9c03f3882edd1b6e870db
johnvattic <mislimumer@gmail.com>
2018-10-19 18:15:02 +0300

    adding everything

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const fs = require('fs');
+const _ = require('lodash')
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+
+if(command === 'add') {
+
+  var note =  notes.addNote(argv.title,argv.body);
+
+  if(typeof note === 'undefined') {
+    console.log('note already exists!');
+  }else{
+    console.log('note saved!');
+
+  }
+
+} else if (command === 'list') {
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+
+  allNotes.forEach( (note) => notes.logNote(note));
+
+} else if (command === 'read') {
+  var note = notes.getNote(argv.title);
+  if(note) {
+    console.log("Note Found!");
+    notes.logNote(note);
+    }else{
+    console.log("Note not found");
+  }
+
+
+} else if (command === 'remove') {
+
+  var noteRemoved =  notes.removeNote(argv.title);
+  var message = noteRemoved?"Note Deleted!":"Note not found!";
+
+  console.log(message);
+  console.log(noteRemoved);
+
+} else {
+  console.log('Command not recognized!');
+}
